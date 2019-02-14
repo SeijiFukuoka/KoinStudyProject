e6e1941a8d72a7a387e9c76dd478ba779c6b2365
Seiji Fukuoka <seijifukuoka@gmail.com>
2019-02-13 22:07:45 -0200

    Add custom koin logger

--- app/src/main/java/br/com/android/seiji/koinstudyproject/CustomApplication.kt
+++ app/src/main/java/br/com/android/seiji/koinstudyproject/CustomApplication.kt
@@ -2,11 +2,18 @@ package br.com.android.seiji.koinstudyproject
 
 import android.app.Application
 import org.koin.android.ext.android.startKoin
+import timber.log.Timber
 
 class CustomApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        startKoin(this, listOf(applicationModule, browseModule), loadProperties = true)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        startKoin(
+            this, listOf(applicationModule, browseModule), loadProperties = true,
+            logger = KoinLogger()
+        )
     }
 }
